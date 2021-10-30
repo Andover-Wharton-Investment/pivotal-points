6f19a532d354a6cde8bbda2191355f9a9b983052
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2021-10-30 12:45:29 -0400

    calculates number of passes

--- gen_checks.py
+++ gen_checks.py
@@ -2,20 +2,29 @@ import pandas as pd
 from scipy.stats import linregress
 import numpy as np
 
-df = pd.read_csv("usable_stocks.txt", header=None, names=["Stock"])
+df = pd.read_csv("usable_stocks.txt", header=None, names=["Stock"]).iloc[:10]
 
 def trend(ticker):
-    print(ticker)
     df = pd.read_csv("stocks/{}.csv".format(ticker), index_col=0)
     opens = df['Open'].iloc[-60:].values
     slope, intercept, r_value, p_value, std_err = linregress(range(len(opens)), opens)
     return slope > 0
 
-checks = {'Trend':  trend}
+def volume(ticker):
+    df = pd.read_csv("stocks/{}.csv".format(ticker), index_col=0)
+    volumes = df['Volume'].iloc[-60:].values
+    return np.mean(volumes) * 1.15 <= np.mean(volumes[-5:])
+
+checks = {'Trend':  trend }
 
 
 for check in checks:
     print("Checking {}".format(check))
     df[check] = df['Stock'].apply(checks[check])
+
+def passes(row):
+    return np.sum(row.drop(['Stock']).astype(np.int32))
+
+df['Passes'] = df.apply(passes, axis=1)
 
 df.to_csv("stock_list_checks.csv", index=False)
