cb08fe6bd816e037825e5ff89a48b052172a1c83
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2021-10-30 16:26:35 -0400

    new checks added

--- gen_checks.py
+++ gen_checks.py
@@ -15,8 +15,21 @@ def volume(ticker):
     volumes = df['Volume'].iloc[-60:].values
     return np.mean(volumes) * 1.15 <= np.mean(volumes[-5:])
 
-checks = {'Trend':  trend, 'Volume': volume}
+def intervals(ticker):
+    INTERVALS = [(100, 105), (200, 205), (300, 305), (400, 405), (500, 510), (1000, 1010), (1500, 1520), (2000, 2020), (2500, 2520), (3000, 3030), (3500, 3530), (4000, 4040), (4500, 4540), (5000, 5050)]
+    df = pd.read_csv("stocks/{}.csv".format(ticker), index_col=0)
+    closes = df['Close'].iloc[-3:].values
+    counts = 0
+    for close in closes:
+        for interval in INTERVALS:
+            if interval[0] <= close <= interval[1]:
+                return True
+    two_year_high = df['High'].iloc[-500:].max()
+    five_day_high = df['High'].iloc[-5:].max()
+    return five_day_high == two_year_high
 
+
+checks = {'Trend':  trend, 'Volume': volume, 'Hundreds': intervals}
 
 for check in checks:
     print("Checking {}".format(check))
