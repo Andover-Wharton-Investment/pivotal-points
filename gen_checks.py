e35ca7c71e8e3e7ebc95443c9767be8f201c931e
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2021-10-30 12:48:58 -0400

    volume check added

--- gen_checks.py
+++ gen_checks.py
@@ -2,7 +2,7 @@ import pandas as pd
 from scipy.stats import linregress
 import numpy as np
 
-df = pd.read_csv("usable_stocks.txt", header=None, names=["Stock"]).iloc[:10]
+df = pd.read_csv("usable_stocks.txt", header=None, names=["Stock"])
 
 def trend(ticker):
     df = pd.read_csv("stocks/{}.csv".format(ticker), index_col=0)
@@ -15,7 +15,7 @@ def volume(ticker):
     volumes = df['Volume'].iloc[-60:].values
     return np.mean(volumes) * 1.15 <= np.mean(volumes[-5:])
 
-checks = {'Trend':  trend }
+checks = {'Trend':  trend, 'Volume': volume}
 
 
 for check in checks:
@@ -26,5 +26,7 @@ def passes(row):
     return np.sum(row.drop(['Stock']).astype(np.int32))
 
 df['Passes'] = df.apply(passes, axis=1)
+
+df.sort_values('Passes', ascending=False, inplace=True)
 
 df.to_csv("stock_list_checks.csv", index=False)
