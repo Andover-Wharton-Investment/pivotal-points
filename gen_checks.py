39fb857e0aff5ed4216788265a89fc19a9fc9355
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2021-10-30 17:58:58 -0400

    only large stocks and updated stock list

--- gen_checks.py
+++ gen_checks.py
@@ -6,14 +6,14 @@ df = pd.read_csv("usable_stocks.txt", header=None, names=["Stock"])
 
 def trend(ticker):
     df = pd.read_csv("stocks/{}.csv".format(ticker), index_col=0)
-    opens = df['Open'].iloc[-60:].values
+    opens = df['Open'].iloc[-20:].values
     slope, intercept, r_value, p_value, std_err = linregress(range(len(opens)), opens)
-    return slope > 0
+    return 20 * slope / df['Open'].iloc[-20] >= 0.1
 
 def volume(ticker):
     df = pd.read_csv("stocks/{}.csv".format(ticker), index_col=0)
-    volumes = df['Volume'].iloc[-60:].values
-    return np.mean(volumes) * 1.15 <= np.mean(volumes[-5:])
+    volumes = df['Volume'].iloc[-60:].values * df['Close'].iloc[-60:].values
+    return np.mean(volumes) * 1.2 <= np.mean(volumes[-5:])
 
 def intervals(ticker):
     INTERVALS = [(100, 105), (200, 205), (300, 305), (400, 405), (500, 510), (1000, 1010), (1500, 1520), (2000, 2020), (2500, 2520), (3000, 3030), (3500, 3530), (4000, 4040), (4500, 4540), (5000, 5050)]
--- getStockDatabase.py
+++ getStockDatabase.py
@@ -5,4 +5,10 @@ with open('stock_list.txt') as f:
     for line in f:
         stock = yf.Ticker(line.strip())
         print(line)
-        stock.history(period='max').to_csv('stocks/' + line.strip() + '.csv')
+        try:
+            if stock.info['marketCap'] is not None and stock.info['marketCap'] > 2000000000:
+                stock.history(period='max').to_csv('stocks/' + line.strip() + '.csv')
+                with open('usable_stocks.txt', 'a') as usable_stocks:
+                    usable_stocks.write(line)
+        except KeyError:
+            print('KeyError')
