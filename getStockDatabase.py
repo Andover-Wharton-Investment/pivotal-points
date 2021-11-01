6ec3d81ebef0ffc10e6688ca7e9a31036aca9d62
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2021-11-01 17:30:48 -0400

    removes duplicates

--- getStockDatabase.py
+++ getStockDatabase.py
@@ -1,14 +1,18 @@
 import yfinance as yf
 import pandas as pd
 
+used = []
+
 with open('stock_list.txt') as f:
     for line in f:
-        stock = yf.Ticker(line.strip())
-        print(line)
-        try:
-            if stock.info['marketCap'] is not None and stock.info['marketCap'] > 2000000000:
-                stock.history(period='max').to_csv('stocks/' + line.strip() + '.csv')
-                with open('usable_stocks.txt', 'a') as usable_stocks:
-                    usable_stocks.write(line)
-        except KeyError:
-            print('KeyError')
+        if line.strip() not in used:
+            used.append(line.strip())
+            stock = yf.Ticker(line.strip())
+            print(line)
+            try:
+                if stock.info['marketCap'] is not None and stock.info['marketCap'] > 2000000000:
+                    stock.history(period='max').to_csv('stocks/' + line.strip() + '.csv')
+                    with open('usable_stocks.txt', 'a') as usable_stocks:
+                        usable_stocks.write(line)
+            except KeyError:
+                print('KeyError')
