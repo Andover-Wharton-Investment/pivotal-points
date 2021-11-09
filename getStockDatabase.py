234971ca2a5455c5e44b2ddc11d58fa6c2133f78
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2021-11-08 20:04:27 -0500

    fixed jsondecodeerror

--- getStockDatabase.py
+++ getStockDatabase.py
@@ -1,18 +1,25 @@
 import yfinance as yf
 import pandas as pd
+import json
 
 used = []
 
 with open('stock_list.txt') as f:
     for line in f:
         if line.strip() not in used:
-            used.append(line.strip())
-            stock = yf.Ticker(line.strip())
             print(line)
-            try:
-                if stock.info['marketCap'] is not None and stock.info['marketCap'] > 2000000000:
-                    stock.history(period='max').to_csv('stocks/' + line.strip() + '.csv')
-                    with open('usable_stocks.txt', 'a') as usable_stocks:
-                        usable_stocks.write(line)
-            except KeyError:
-                print('KeyError')
+            used.append(line.strip())
+            while True:
+                try:
+                    stock = yf.Ticker(line.strip())
+                
+                    if stock.info['marketCap'] is not None and stock.info['marketCap'] > 2000000000:
+                        stock.history(period='max').to_csv('stocks/' + line.strip() + '.csv')
+                        with open('usable_stocks.txt', 'a') as usable_stocks:
+                            usable_stocks.write(line)
+                    break
+                except KeyError:
+                    print('KeyError')
+                    break
+                except json.decoder.JSONDecodeError:
+                    print('JSONDecodeError')
