575747f0d692cf4d0b08736696166facb6523041
cturk23 <93450388+cturk23@users.noreply.github.com>
2021-11-28 22:27:59 -0600

    changed -20 in range to -3 for time component skippage

--- get_historical_correlation.py
+++ get_historical_correlation.py
@@ -49,7 +49,7 @@ with open('usable_stocks.txt', 'r') as f:
         num_rows = len(cur_df.index)
         try:
             end_date = cur_df.index[cur_df['Date'] == '2019-12-31'].tolist()[0]
-            for i in range(end_date, 60, -20):
+            for i in range(end_date, 60, -3):
                 df.loc[row_num] = [cur_df['Date'][i], ticker, trend_twenty_day(cur_df.iloc[:i+1]), trend_sixty_day(cur_df.iloc[:i+1]), volume(cur_df.iloc[:i+1]), rsi(cur_df.iloc[:i+1]), np.round(cur_df['Close'][i+5] / cur_df['Close'][i] - 1, 4), np.round(cur_df['Close'][i+20] / cur_df['Close'][i] - 1, 4), np.round(cur_df['Close'][i+60] / cur_df['Close'][i] - 1, 4), np.round(cur_df['Close'][i+252] / cur_df['Close'][i] - 1, 4)]
                 row_num += 1
         except IndexError:
