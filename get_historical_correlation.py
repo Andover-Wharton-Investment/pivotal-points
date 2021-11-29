799135d64f682e792e0bcbf81f8573a9142dd726
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2021-11-29 09:29:13 -0500

    try to optimize

--- get_historical_correlation.py
+++ get_historical_correlation.py
@@ -38,22 +38,35 @@ def rsi(df):
     rsi_final = (100 - (100 / (1+rs)))*0.01
     return ((-1)*(rsi_final-1)*2)-1
 
-df = pd.DataFrame(columns=['Date', 'ticker', 'trend_twenty_day', 'trend_sixty_day', 'volume', 'rsi', '5-day return', '20-day return', '60-day return', 'year return'])
+dfs = {}
 
+def get_cols(ticker, i):
+    cur_df = dfs[ticker]
+    return {"20 day trend": trend_twenty_day(cur_df.iloc[:i+1]), "60 day trend": trend_sixty_day(cur_df.iloc[:i+1]), "Volume": volume(cur_df.iloc[:i+1]), "RSI": rsi(cur_df.iloc[:i+1]), "5 day return": np.round(cur_df['Close'][i+5] / cur_df['Close'][i] - 1, 4), "20 day return": np.round(cur_df['Close'][i+20] / cur_df['Close'][i] - 1, 4), "60 day return": np.round(cur_df['Close'][i+60] / cur_df['Close'][i] - 1, 4), "One year return": np.round(cur_df['Close'][i+252] / cur_df['Close'][i] - 1, 4)}
+
+df = pd.DataFrame(columns=['i', 'Date', 'ticker'])
+
+print("Dates")
 with open('usable_stocks.txt', 'r') as f:
     row_num = 0
     for line in f:
         ticker = line.strip()
         print(ticker)
         cur_df = pd.read_csv("stocks/{}.csv".format(ticker))
+        dfs[ticker] = cur_df
         num_rows = len(cur_df.index)
         try:
             end_date = cur_df.index[cur_df['Date'] == '2019-12-31'].tolist()[0]
-            for i in range(end_date, 60, -20):
-                df.loc[row_num] = [cur_df['Date'][i], ticker, trend_twenty_day(cur_df.iloc[:i+1]), trend_sixty_day(cur_df.iloc[:i+1]), volume(cur_df.iloc[:i+1]), rsi(cur_df.iloc[:i+1]), np.round(cur_df['Close'][i+5] / cur_df['Close'][i] - 1, 4), np.round(cur_df['Close'][i+20] / cur_df['Close'][i] - 1, 4), np.round(cur_df['Close'][i+60] / cur_df['Close'][i] - 1, 4), np.round(cur_df['Close'][i+252] / cur_df['Close'][i] - 1, 4)]
+            for i in range(end_date, 60, -5):
+                df.loc[row_num] = [i, cur_df['Date'][i], ticker]
                 row_num += 1
         except IndexError:
             pass
+
+print("Applying")
+
+appiled_df = df.apply(lambda row: get_cols(row.ticker, row.i), axis='columns', result_type='expand')
+df = pd.concat([df.drop(columns=['i']), appiled_df], axis='columns')
 
 df.sort_values('Date', inplace=True)
 df.to_csv('historical_correlation.csv', index=False)
